aafe3813e099b7e39673d0425ffb837c27023d98
sjurzi <sjur.foyen@gmail.com>
2023-03-09 15:25:43 +0100

    SOGI PLL parametric study

--- models/sogi_pll_fll.py
+++ models/sogi_pll_fll.py
@@ -1,5 +1,8 @@
 import numpy as np
 from sympy import symbols, cos, sin, Matrix, solve, simplify
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
 from hss import HSS
 from htf import HTF
 from parametric_studies import ParametricSweep
@@ -30,8 +33,8 @@ class SogiPll(HSS):
         kp = 2 * a_pll
         ki = 2 * a_pll ** 2
         th_pll = xd + self.w0 * self.t
-        ffp1 = True  # Integrator is before multiplication with omega
-        ffp2 = True  # Integrator is before multiplication with omega
+        ffp1 = False  # Integrator is before multiplication with omega
+        ffp2 = False  # Integrator is before multiplication with omega
         # TODO: support for algebraic loops (in a DAE framework)
         # Solve loop symbolically
         wpll = symbols('wpll')
@@ -140,30 +143,43 @@ class SogiFll(HSS):
         self.y = [w_fll]
 
 
-sogi = SogiFll()
+# Declare model and do parametric sweep
+sogi = SogiPll()
 sogi.find_pss()
 param_sweep = ParametricSweep(sogi)
 param_sweep.sweep([np.flip(np.linspace(0.2, 5, 30)), np.flip(np.linspace(20, 150, 30))])
-#ax = param_sweep.plot_parametric_study3d(offset=-200)
-import matplotlib.pyplot as plt
-from matplotlib import cm
+
+
+# plot 3d
+ax = param_sweep.plot_parametric_study3d(offset=-200)
+ax.set_xlabel(r'$k_{sog}$',fontsize=20)
+ax.set_ylabel(r'$\alpha_{fll}$',fontsize=20, labelpad=15)
+ax.set_zlabel(r'$Re[\lambda]$',fontsize=20, labelpad=15)
+ax.set_xticks([0, 2, 5])
+ax.set_yticks([0, 50, 100,150])
+#ax.set_zticks([-200,-100,0])
+ax.tick_params(labelsize=17)
+ax.view_init(20,120)
+plt.savefig("sogi_pll_3d_00.svg", bbox_inches="tight",
+            pad_inches=0.0, transparent=True, format='svg', dpi=600)
+
 
 fig, ax = plt.subplots(1)
 levels = np.linspace(-200,0,21)
 
 contourf = param_sweep.weakest_damping_contourf(ax, levels=levels, extend='both', cmap=cm.coolwarm)
 param_sweep.weakest_damping_hatches(ax, levels=[-150, -20, 0, 1e10], colors='none', hatches=[None, '/', '++'])
+#cbar = fig.colorbar(contourf, ticks=np.linspace(levels[0], levels[-1], 5))
+#cbar.ax.tick_params(labelsize=20)
 
-cbar = fig.colorbar(contourf, ticks=np.linspace(levels[0], levels[-1], 5))
-
-ax.set_xlabel(r'$k_{sog}$',fontsize=15)
-ax.set_ylabel(r'$\alpha_{fll}$',fontsize=15)
+ax.set_xlabel(r'$k_{sog}$',fontsize=25)
+ax.set_ylabel(r'$\alpha_{fll}$',fontsize=25)
 #ax.set_zlabel(r'$Re[\lambda]$',fontsize=15)
 ax.set_xticks([0.2, 2.5, 5])
 ax.set_yticks([20, 50, 100,150])
 #ax.set_zticks([-200,-100,0])
-ax.tick_params(labelsize=12)
+ax.tick_params(labelsize=20)
 #ax.view_init(10,120)
-plt.savefig("sogi_fll_2d_00.svg", bbox_inches="tight",
+plt.savefig("sogi_pll_2d_00.svg", bbox_inches="tight",
             pad_inches=0.3, transparent=True, format='svg', dpi=600)
 
